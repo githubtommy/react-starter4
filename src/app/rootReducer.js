4f65b5dd3731d3a340c9c54117ebce476a7033e5
Tom Beaver <sourcetree@tombeaver.com>
2017-07-13 15:35:44 -0500

    remove some references to deleted components

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,17 +1,7 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
-import counter from '../scenes/counter/counterReducer'
 import { randomNameObj } from '../scenes/lab/randomName/reducers/randomNameReducer.js'
-import { factsObj } from '../scenes/weknow/reducers/weKnowReducer.js';
-import { feedbackObj } from '../scenes/feedbackr/reducers/feedbackReducer.js';
-import { domainFilterObj } from '../scenes/feedbackr/reducers/fbhome_reducer.js';
-import { topicFilterObj } from '../scenes/feedbackr/reducers/fbhome_reducer.js';
 export default combineReducers({
-	feedbackObj,
-	domainFilterObj,
-	topicFilterObj,
 	randomNameObj,
-  router: routerReducer,
-  counter,
-	factsObj
+  router: routerReducer
 })
